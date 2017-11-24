25bb70426767bf9f9843b5e67edd796c6e27ad58
Cleber Santos <oclebersantos@gmail.com>
2017-11-24 18:37:53 -0200

    [Update] Parerista/Analise Inicial: plano de distribuicao e planilha orcamentaria #59

--- application/modules/default/controllers/IndexController.php
+++ application/modules/default/controllers/IndexController.php
@@ -354,4 +354,37 @@ class IndexController extends MinC_Controller_Action_Abstract
             )
         );
     }
+
+    public function montarPlanilhaOrcamentariaAjaxAction()
+    {
+
+        $this->_helper->layout->disableLayout(); // desabilita o Zend_Layout
+        $get = Zend_Registry::get('get');
+
+        $GrupoAtivo = new Zend_Session_Namespace('GrupoAtivo'); // cria a sessao com o grupo ativo
+        $this->view->idPerfil = $GrupoAtivo->codGrupo;
+
+        $this->view->idPronac = $get->idPronac;
+        $spPlanilhaOrcamentaria = new spPlanilhaOrcamentaria();
+        $planilhaOrcamentaria = $spPlanilhaOrcamentaria->exec($get->idPronac, $get->tipoPlanilha);
+        $planilha = $this->montarPlanilhaOrcamentaria($planilhaOrcamentaria, $get->tipoPlanilha);
+        // tipoPlanilha = 0 : Planilha Orcamentaria da Proposta
+        // tipoPlanilha = 1 : Planilha Orcamentaria do Proponente
+        // tipoPlanilha = 2 : Planilha Orcamentaria do Parecerista
+        // tipoPlanilha = 3 : Planilha Orcamentaria Aprovada Ativa
+        // tipoPlanilha = 4 : Cortes Orcamentarios Aprovados
+        // tipoPlanilha = 5 : Remanejamento menor que 20%
+        // tipoPlanilha = 6 : Readequacao
+
+        $link = isset($get->link) ? true : false;
+
+        $this->montaTela(
+            'index/montar-planilha-orcamentaria.phtml', array(
+                'tipoPlanilha' => $get->tipoPlanilha,
+                'tpPlanilha' => (count($planilhaOrcamentaria)>0) ? isset($planilhaOrcamentaria[0]->tpPlanilha) ? $planilhaOrcamentaria[0]->tpPlanilha : '' : '',
+                'planilha' => $planilha,
+                'link' => $link
+            )
+        );
+    }
 }
